49b8a3c1622738cc7e4e970ea97533d1b99764c3
calder3 <71357100+calder3@users.noreply.github.com>
2020-10-21 11:40:59 -0700

    Add files via upload

--- Quadratic_equation_solver.py
+++ Quadratic_equation_solver.py
@@ -10,13 +10,17 @@ class Quadratic_Equation:
 
     def root1(self):
             if self.discriminant() < 0:
+            	self.root1 = f'({-self.b} + sqrt({self.discriminant()}))/{2*self.a}'
             	return f'({-self.b} + sqrt({self.discriminant()}))/{2*self.a}'
-            else:		
+            else:
+            	self.root1 = (-self.b + math.sqrt(self.discriminant())) / (2 * self.a)		
             	return (-self.b + math.sqrt(self.discriminant())) / (2 * self.a)
     def root2(self):
             if self.discriminant() < 0:
+            	self.root2 = f'({-self.b} - sqrt({self.discriminant()}))/{2*self.a}'
             	return f'({-self.b} - sqrt({self.discriminant()}))/{2*self.a}'            	
             else:
+            	self.root2 = (-self.b - math.sqrt(self.discriminant())) / (2 * self.a)
             	return (-self.b - math.sqrt(self.discriminant())) / (2 * self.a)
     def __str__(self):
     	return (self.root1)
@@ -29,9 +33,15 @@ class Quadratic_Equation:
     	k = self.c - (h**2)
     	return f'{self.a}(x-{h})+{k}'
 
+    def facoted_form(self):
+    	return f'(x-{self.root1})(x-{self.root2})'
 
-q1 = Quadratic_Equation(1,4,1)
+
+q1 = Quadratic_Equation(1,4,4)
 print(q1.virtex_form())
+print(q1.root1())
+print(q1.root2())
+print(q1.facoted_form())
 
 
 
